b54aa95d50ae0d068dde2fd7a35ed80be6a031f0
miguel <medina@stetter-itq.com>
2019-11-13 15:08:37 +0100

    last commit

--- validation.py
+++ validation.py
@@ -1,7 +1,6 @@
 from rasa_nlu.model import Metadata, Interpreter
-
 if __name__ == "__main__":    
     # where `model_directory points to the folder the model is persisted in
-    interpreter = Interpreter.load('C:/Users/DrStetterITQ/Desktop/sylvia/models')
+    interpreter = Interpreter.load('C:\\Users\\DrStetterITQ\\Desktop\\sylvia\\models\\nlu')
 
-    interpreter.parse(u"The text I want to understand")    
+    print(interpreter.parse("get a quick quote"))
